b3126e0fb14d28dc254bf9a830e08b3ed4458e8a
Nadia M <siraniadam@me.com>
2020-04-19 08:36:26 +0200

    ok

---
+++ Helpers/constants.swift
@@ -0,0 +1,19 @@
+//
+//  constants.swift
+//  App1FirebasAuth
+//
+//  Created by Nadia Mettioui on 18/04/2020.
+//  Copyright © 2020 Nadia Mettioui. All rights reserved.
+//
+
+import Foundation
+
+
+struct Constants {
+    
+    
+    struct Storyboars {
+        static let HomeViewController = "HomeVC"
+        
+    }
+}
---
+++ ViewsController/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  App1FirebasAuth
+//
+//  Created by Nadia Mettioui on 19/04/2020.
+//  Copyright © 2020 Nadia Mettioui. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
